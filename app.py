0622b1024eafb164339d66d00c74878f226a6969
vaibhavdond <60029073+vaibhavdond@users.noreply.github.com>
2021-04-25 10:56:10 +0530

    feat
    added a new method to search restaurant by co-ordinates i.e. latitude and longitude

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from firebase_admin import firestore
 import flask
 from flask import abort, jsonify, request, redirect
 
+import math
+from math import sin, cos, sqrt, atan2, radians
+
 import datetime
 import json
 import requests
@@ -45,6 +48,34 @@ def search_restaurant():
     for val in arr:
         restaurants_list.append(val.to_dict())
     return jsonify({"Response" : 200, "Restaurants_list" : restaurants_list})
+
+@app.route('/search_restaurant/by_coordinates', methods=['POST'])
+def search_restaurant():
+    data = request.get_json(force=True)
+    mylat = data.get("latitude")
+    mylon = data.get("longitude")
+    r = data.get("range")
+
+    # approximate radius of earth in km
+    R = 6373.0
+
+    arr = store.collection("RESTAURANTS").stream()
+    restaurants_near_me = []
+    for restaurant in arr:
+        rdict = restaurant.to_dict()
+        rlat = radians(rdict.get("location").get("latitude"))
+        rlon = radians(rdict.get("location").get("longitude"))
+
+        dlon = rlon - mylon
+        dlat = rlat - mylat
+
+        a = sin(dlat / 2)**2 + cos(mylat) * cos(rlat) * sin(dlon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        distance = R * c
+
+        if(distance<=r):
+            restaurants_near_me.append(rdict)
+    return jsonify({"Response" : 200, "Restaurants_near_me" : restaurants_near_me})
 
 if __name__ == '__main__':
     app.run(debug=False)
